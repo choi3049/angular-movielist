68e2a70a2be802e35651df863d82e65d51b5bb24
CHOE BYEONGSEON <qudtjs3012@naver.com>
2022-05-09 16:34:02 +0900

    Add list (*ngIf )

--- src/app/movies/movie-list.component.ts
+++ src/app/movies/movie-list.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-movies',
-  templateUrl: './movie-list.component.html',
+  selector: "app-movies",
+  templateUrl: "./movie-list.component.html",
 })
 export class MovieListComponent {
-  subTitle: string = '映画リスト';
+  subTitle: string = "映画リスト";
+  movies: any[] = [
+    {
+      movieId: 1,
+      name: "matrix4",
+      director: "aa",
+      releaseDate: "2022-01-10",
+      actor: "Keanu Reevas",
+      rate: 4,
+      imageUrl: "",
+    },
+    {
+      movieId: 2,
+      name: "matrix4",
+      director: "aa",
+      releaseDate: "2022-01-10",
+      actor: "Keanu Reevas",
+      rate: 3,
+      imageUrl: "",
+    },
+  ];
 }
