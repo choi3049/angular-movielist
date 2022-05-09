6c848022db97c67e36bc0bfb7683ffcfdb8b7e90
CHOE BYEONGSEON <qudtjs3012@naver.com>
2022-05-09 16:17:44 +0900

    Create a Frame

---
+++ src/app/movies/movie-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movie-list.component.html',
+})
+export class MovieListComponent {
+  subTitle: string = '映画リスト';
+}
